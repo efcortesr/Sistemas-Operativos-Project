00cd3ad338a63648591a80c67cfd589be4278b5e
efcortesr <159450837+efcortesr@users.noreply.github.com>
2025-08-05 19:24:44 -0500

    Version actualizada proyecto1

---
+++ proyecto1SO/include/proceso.h
@@ -0,0 +1,13 @@
+#ifndef PROCESO_H
+#define PROCESO_H
+#include <string>
+#include <vector>
+struct Proceso{
+	int pid;
+	int pc;
+	int ax, bx, cx;
+	int quantum;
+	std::string estado;
+	std::vector<std::string> instrucciones;
+};
+#endif
